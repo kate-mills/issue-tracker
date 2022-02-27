762284990856ebfa173680bd23926899d96c002d
Kate Mills <helloworldloveksm@icloud.com>
2022-02-27 14:03:28 -0800

    Test 'Update an issue with an invalid _id'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -199,6 +199,9 @@ suite('Functional Tests', function() {
       })
       .end(function(err, res) {
         assert.equal(res.status, 200);
+        assert.equal(res.body.error, 'could not update')
+        assert.equal(res.body._id, 'abc123');
+
         done();
       });
   });
